412ce2b6cdbf7ad25c9328e84910de781f9fe87b
samuelssnider <sam_redins@yahoo.com>
2017-09-14 09:39:53 -0600

    test 2 working

--- spec/features/user_can_visit_a_vending_machine_show_and_see_average_price_of_snacks_spec.rb
+++ spec/features/user_can_visit_a_vending_machine_show_and_see_average_price_of_snacks_spec.rb
@@ -1,11 +1,10 @@
 require 'rails_helper'
 RSpec.describe "As a user" do
 	describe "I can visit a vending machine show page" do
-		it "and see all snacks and their associated prices" do
+		it "and see average price of all snacks" do
 			create_one
 			visit machine_path(@machine)
-			expect(page).to have_content(@snack_one.name)
-			expect(page).to have_content(@snack_two.name)
+			expect(page).to have_content((@machine.average_price.to_f) / 100)
 		end
 	end
 end
--- spec/helpers.rb
+++ spec/helpers.rb
@@ -1,10 +1,10 @@
 module Helpers 
 	def create_one
 		@owner = Owner.create(name: "Sam's Snacks")
-		sam.machines.create(location: "Don's Mixed Drinks")
-		@machine = sam.machines.first
+		@owner.machines.create(location: "Don's Mixed Drinks")
+		@machine = @owner.machines.first
 		@machine.snacks.create(name:  "White Castle Burger", price: 350)
-		@machine.snacks.create(name:  "Flaming Hot Cheetos" price: 250)
+		@machine.snacks.create(name:  "Flaming Hot Cheetos", price: 250)
 		@snack_one = @machine.snacks.first
 		@snack_two = @machine.snacks.last
 	end
